2a3f1fd53474253f83c92c8094c09fa5f4191cc9
kris <bukovi@me.com>
2018-07-24 06:07:07 -0400

    edit tf_nn file to grab values from spreadsheet and display them

--- tf_nn.py
+++ tf_nn.py
@@ -1,19 +1,17 @@
 # filename: tf_nn.py
 # author: kris bukovi
-# last modified: July 21, 2018
+# last modified: July 24, 2018
 
-import math
-import numpy as np
-import h5py
-import matplotlib.pyplot as plt
-import tensorflow as tf
-from tensorflow.python.framework import ops
+
 from openpyxl import load_workbook
+import pandas as pd
 
 def load_dataset():
 
     lat = []
     lon = []
+    ave = []
+    limit = []
     
     # load workbook
     wb = load_workbook('test.xlsx')
@@ -27,36 +25,46 @@ def load_dataset():
     for i in range(4, 849):
 
         # store latitude
-        lat.append(ws['E' + str(i)])
+        lat.append(ws['E' + str(i)].value)
 
         # store longitude
-        lon.append(ws['F' + str(i)])
+        lon.append(ws['F' + str(i)].value)
+
+        # store average of draws
+        ave.append(ws['L' + str(i)].value)
+
+        # store over limit value
+        limit.append(ws['M' + str(i)].value)
 
     # display locations on chart
+    df = pd.DataFrame({'latitude': lat, 'longitude': lon, 'average': ave, 'over limit?': limit})
+    print(df)
 
 
 
 
-def random_mini_batches(X, Y, mini_batch_size = 64, seed = 0):
+#def random_mini_batches(X, Y, mini_batch_size = 64, seed = 0):
 
-def convert_to_one_hot(Y, C):
+#def convert_to_one_hot(Y, C):
 
-def predict(X, parameters):
+#def predict(X, parameters):
 
-def forward_propagation_for_predict(X, parameters):
+#def forward_propagation_for_predict(X, parameters):
 
-def one_hot_matrix(labels, C):
+#def one_hot_matrix(labels, C):
 
-def ones(shape):
+#def ones(shape):
 
-def create_placeholders(n_x, n_y):
+#def create_placeholders(n_x, n_y):
 
-def initialize_parameters():
+#def initialize_parameters():
 
-def forward_propagation(X, parameters):
+#def forward_propagation(X, parameters):
 
-def compute_cost(Z3, Y):
+#def compute_cost(Z3, Y):
 
-def model(X_train, Y_train, X_test, Y_test, learning_rate = 0.0001,
-            num_epochs = 1500, minibatch_size = 32, print_cost = True):
+#def model(X_train, Y_train, X_test, Y_test, learning_rate = 0.0001,
+            #num_epochs = 1500, minibatch_size = 32, print_cost = True):
+
+load_dataset()
 
