0dd8bdc04349e515903ac6767109c6aba687c03d
kris <bukovi@me.com>
2018-07-24 05:06:34 -0400

    finishing over limit file to determine if average draw is over 5 ppb

--- over_limit.py
+++ over_limit.py
@@ -19,19 +19,42 @@ ws = wb[first_sheet]
 
 for i in range(4,849):
 
+    num_draws = 3
+
     try:
 
         # store first draw
-        first_draw = ws['G' + str(i)]
+        first_draw = ws['G' + str(i)].value
+
+        if first_draw == "<0.3":
+            first_draw = "0.3"
+        elif first_draw == "<1":
+            first_draw = "1.0"
+        elif first_draw == "":
+            num_draws-=1
 
         # store fourth draw
-        fourth_draw = ws['H' + str(i)]
+        fourth_draw = ws['H' + str(i)].value
+
+        if fourth_draw == "<0.3":
+            fourth_draw = "0.3"
+        elif fourth_draw == "<1":
+            fourth_draw = "1.0"
+        elif fourth_draw == "":
+            num_draws-=1
 
         # store sixth draw
-        sixth_draw = ws['I' + str(i)]
+        sixth_draw = ws['I' + str(i)].value
+
+        if sixth_draw == "<0.3":
+            sixth_draw = "0.3"
+        elif sixth_draw == "<1":
+            sixth_draw = "1.0"
+        elif sixth_draw == "":
+            num_draws-=1
 
         # get the average 
-        average = (first_draw + fourth_draw + sixth_draw) / 3
+        average = (float(first_draw) + float(fourth_draw) + float(sixth_draw)) / num_draws
 
         # write average to file
         ws['L' + str(i)] = average
@@ -45,8 +68,8 @@ for i in range(4,849):
         # save file
         wb.save('test.xlsx')
 
-    except:
-        print("Can't calculate average, skipping...")
+    except Exception as e:
+        print(e)
         
 
 
